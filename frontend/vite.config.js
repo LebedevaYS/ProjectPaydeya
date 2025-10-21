c46a28a313302665f19628cbc7688cd7c360c5e0
LebedevaYS <lebedeva.ys@outlook.com>
2025-10-21 16:43:24 +0300

    добавлена связь с бэком

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'import.meta.env.VITE_API_URL': JSON.stringify('https://paydeya-backend.onrender.com')
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://paydeya-backend.onrender.com',
+        changeOrigin: true,
+        secure: false,
+        rewrite: (path) => path.replace(/^\/api/, '/api')
+      }
+    }
+  }
 })
